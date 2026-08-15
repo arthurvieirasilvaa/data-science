098f88f97884c4948ff5f3d1f36d18f8ca85bfe0
Arthur Vieira Silva <arthursilvavieira03@gmail.com>
2026-08-15 10:20:46 -0300

    Implementei o cálculo da Distância Euclidiana entre dois pontos

--- k-means/src/main.py
+++ k-means/src/main.py
@@ -1,8 +1,24 @@
 import csv
+import math
 from pathlib import Path
+
+# Constantes utilizadas:
+K = 2
+MAX_ITERATIONS = 100
+
+
+def calculate_euclidean_distance(p, q):
+    """Função utilizada para calcular a Distância Euclidiana entre dois pontos."""
+    n = len(p)
+    result = 0
+    for i in range(n):
+        result += pow(p[i]-q[i], 2)
+
+    return math.sqrt(result)
 
 
 def read_input_file(file_name):
+    """Função utilizada para ler o arquivo csv de entrada e retornar os pontos obtidos."""
     file_path = Path("k-means/input") / file_name
 
     points = []
@@ -22,6 +38,7 @@ def read_input_file(file_name):
 def run():
     points = read_input_file("dados_1_simples.csv")
     print(points)
+
 
 if __name__ == '__main__':
     run()

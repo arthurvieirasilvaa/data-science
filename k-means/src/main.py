fb46537e649e4e4e6faf4bcaee1ad3030cf3aa1b
Arthur Vieira Silva <arthursilvavieira03@gmail.com>
2026-08-15 10:08:00 -0300

    Criei os pontos a partir do arquivo csv

--- k-means/src/main.py
+++ k-means/src/main.py
@@ -1,16 +1,27 @@
 import csv
 from pathlib import Path
 
+
 def read_input_file(file_name):
     file_path = Path("k-means/input") / file_name
 
-    with open(file_path, 'r', newline='') as f:
-        data = csv.reader(f)
-        for row in data:
-            print(row)
+    points = []
+    try:
+        with open(file_path, 'r', newline='') as f:
+            data = csv.reader(f)
+            for row in data:
+                points.append(tuple(row)) # cria uma tupla com os valores da linha e adiciona na lista de pontos
+
+    except FileNotFoundError as error:
+        print(f"O arquivo {file_name} não existe!: {error}")
+
+    points.pop(0)
+    return points
+
 
 def run():
-    read_input_file("dados_1_simples.csv")
+    points = read_input_file("dados_1_simples.csv")
+    print(points)
 
 if __name__ == '__main__':
     run()

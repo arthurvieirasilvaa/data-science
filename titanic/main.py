dd9f35daace99f6a6115aae087a951becea7398e
Arthur Vieira Silva <arthursilvavieira03@gmail.com>
2026-09-01 09:53:47 -0300

    Implementei a taxa de sobrevivência por faixa etária

--- titanic/main.py
+++ titanic/main.py
@@ -19,6 +19,24 @@ def get_age_distribution_by_sex_and_pclass(df: pd.DataFrame):
     age_stats = {"media": mean_age, "mediana": median_age, "desvio_padrao": std_age}
     return age_stats
 
+def get_survival_rate_by_age_group(df: pd.DataFrame):
+    """Função utilizada para obter a Taxa de Sobrevivência por Faixa Etária."""
+
+    # Criando faixas etárias apropriadas: Crianças, Jovens, Adultos e Idosos:
+    children = df[df['Age']<=12]
+    young_people = df[(df['Age']>12) & (df['Age']<=29)]
+    adults = df[(df['Age']>29) & (df['Age']<=59)]
+    elderly = df[df['Age']>=60]
+
+    # Determinando a taxa proporcional de sobreviventes em cada grupo demográfico:
+    children_rate = children['Survived'].value_counts(normalize=True)
+    young_people_rate = young_people['Survived'].value_counts(normalize=True)
+    adults_rate = adults['Survived'].value_counts(normalize=True)
+    elderly_rate = elderly['Survived'].value_counts(normalize=True)
+
+    rates = {"criancas": children_rate, "jovens": young_people_rate, "adultos": adults_rate, "idosos": elderly_rate}
+    return rates
+
 def run():
     """
         Funçao utilizada para obter a Distribuição de Idade por Gênero e Classe
@@ -35,7 +53,13 @@ def run():
     print("Mediana das Idades:")
     print(f"\t{age_stats['mediana']}\n")
     print("Desvio padrão das Idades:")
-    print(f"\t{age_stats['desvio_padrao']}\n")    
+    print(f"\t{age_stats['desvio_padrao']}\n")
+
+    rates = get_survival_rate_by_age_group(df)
+    print("============= Taxa de Sobrevivência por Faixa Etária =============\n")
+    for key, value in rates.items():
+        print(f"{key}:")
+        print(f"\t{value}\n") 
 
 if __name__ == '__main__':
     run()
